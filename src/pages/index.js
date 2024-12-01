35047b68c3d0d2a846fd7f7a487f898838793dcc
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-12-01 15:54:57 +0600

    change index file

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Header from "../components/Layout/Header";
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <main>
+                <h1>Добро пожаловать в Construction Shop!</h1>
+                <p>Лучший выбор строительных товаров.</p>
+            </main>
+        </>
+    );
+}
